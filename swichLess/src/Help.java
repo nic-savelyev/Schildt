0ae716847d4828c87ea7e0a4abdea58751148b92
rezak18 <savelev.na@mail.ru>
2013-09-07 14:38:58 +0400

    Revert cc5e30c..7164024
    This rolls back to commit cc5e30c9daa3d43d740157bab71f1a4bcd1f9175.

--- swichLess/src/Help.java
+++ swichLess/src/Help.java
@@ -2,31 +2,37 @@ import java.io.IOException;
 
 public class Help {
     public static void main(String[] args) {
+        char choice = '0';
+        char ignore = ' ';
 
-            System.out.println("Help on:");
-            System.out.println("1. if");
-            System.out.println("2. switch");
-            System.out.println("Chose one: ");
+        HelpDemo helpObj = new HelpDemo();
 
-            char choice = ' ';
-            try {
-                choice = (char) System.in.read();
+        for (;;) {
+            helpObj.showMenu();
+
+            do {
+                try {
+                    choice = (char) System.in.read();
+                }
+                catch (IOException e){
+                    System.out.println("Input error!");
+                }
+
+            } while ( helpObj.invalid(choice) );
+
+
+            if (choice == 'q') {
+                break;
             }
-            catch (IOException e){
-                System.out.println("Input error!");
-            }
 
-            switch (choice){
-                case '1':
-                    System.out.println("The if: lalala");
-                    break;
-                case '2':
-                    System.out.println("The switch: lala");
-                    break;
-                default:
-                    System.out.println("Selection not found!");
+            helpObj.showAnswer(choice);
+            choice = '0';
 
-            }
+            System.out.println();
+        }
+
+
+
 
     }
 }
